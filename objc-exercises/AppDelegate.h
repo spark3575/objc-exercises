ac4801677c8d6b2a142bc785f53ffef95eeb1a91
Shin Park <shinpark@shins-mac-mini.home>
2017-04-05 08:07:30 -0500

    Initial Commit

---
+++ objc-exercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-exercises
+//
+//  Created by Shin Park on 4/5/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-exercises/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc-exercises
+//
+//  Created by Shin Park on 4/5/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
